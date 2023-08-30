8e38e47bd92ced1b25238cc34cb29cbc89ea9e88
yapapahub <40893287+yapapahub@users.noreply.github.com>
2023-08-30 10:14:31 -0400

    parser update

--- lambda/nodejs/.aws-sam/build/MyLambdaFunction/helpers/parser.js
+++ lambda/nodejs/.aws-sam/build/MyLambdaFunction/helpers/parser.js
@@ -184,6 +184,7 @@ module.exports = function (item) {
       temp.content.forEach((page,index) => {
         
         if (page.includes('amp-youtube')) {
+          console.log('includes youtube')
           temp.ampyt = true
           subtitle = page.match(/span>(.*?)</)
           videoid = page.match(/videoid=\"(.*?)\"/)[1]
@@ -209,8 +210,12 @@ module.exports = function (item) {
         temp.videos.forEach(video => {
           video.items = video.items.slice(0,2)
         })
+      console.log('videos:',temp.videos.length)
+
+      if (temp.videos.length > 0) {
         temp.ampyt = true
         temp.amp = true
+      }
         // console.log('videos inside:',temp.videos.length)
       }
       
@@ -357,7 +362,7 @@ module.exports = function (item) {
       // console.log('faq',item.faq, item.faq.length)
       if (item.faq.length > 0) temp.faq = item.faq.shift().list
       // if (item.seo && item.seo.clicks == 0 && item.seo.ga_visits == 0) temp.noads = true 
-
+      console.log('amp:',temp.amp)
       return temp
 }    
 
