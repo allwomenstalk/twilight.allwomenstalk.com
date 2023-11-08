6e5474e4d48c74e6b9c82940c2862638c8d2475c
yapapahub <40893287+yapapahub@users.noreply.github.com>
2023-11-08 03:36:17 -0500

    updated list of faq anabled posts and poll results

--- getposts.js
+++ getposts.js
@@ -20,10 +20,10 @@ const marr = [
 ];
 
 // default_pipeline = 'pipeline_comments1000plus'
-default_pipeline = 'pipeline_seo_top'
+// default_pipeline = 'pipeline_seo_top'
 // default_pipeline = 'pipeline_category'
 // default_pipeline = 'pipeline_host'
-// default_pipeline = 'pipeline_faq'
+default_pipeline = 'pipeline_faq'
 // default_pipeline = 'pipeline_realtime'
 // default_pipeline = 'pipeline_name'
 // default_pipeline = 'pipeline_filter'
