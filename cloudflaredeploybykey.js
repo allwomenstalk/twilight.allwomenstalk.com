56832ed5cd8073362211c0f58b76ac4f6545fbcd
yapapahub <40893287+yapapahub@users.noreply.github.com>
2024-04-11 16:23:45 -0400

    cloudflare deploy with api key only

---
+++ cloudflaredeploybykey.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+require('dotenv').config();
+
+// Load the Cloudflare API key from the environment variable
+const cloudflareApiKey = process.env.CLOUDFLARE_API_KEY;
+console.log('Cloudflare API key:', cloudflareApiKey);
+
+if (!cloudflareApiKey) {
+  console.error('Cloudflare API key is not set in environment variables.');
+  process.exit(1);
+}
+
+// Load the data from pages.json
+fs.readFile('cloudflare/pages.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err);
+    return;
+  }
+
+  // Parse the JSON data
+  const pages = JSON.parse(data);
+
+  // Define a recursive function to deploy each page
+  function deployPage(index) {
+    if (index >= pages.length) {
+      console.log("All pages deployed successfully!");
+      return;
+    }
+
+    const page = pages[index];
+    console.log(`Deploying page: ${page.host}...`)
+    const command = `CLOUDFLARE_API_KEY=${cloudflareApiKey} npx wrangler pages deploy _site/${page.host} --branch main --project-name ${page.project}`;
+
+    // Execute the command
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing: ${command}\n`, error);
+        return;
+      }
+
+      console.log(`Output for: ${command}\n`, stdout);
+
+      if (stderr) {
+        console.error(`Error Output for: ${command}\n`, stderr);
+      }
+
+      // Proceed to the next page
+      deployPage(index + 1);
+    });
+  }
+
+  // Start deploying from the first page
+  deployPage(0);
+});
