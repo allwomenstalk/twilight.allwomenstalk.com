ece5f563f6e66f68105abeab7b0ff73e5296d2b3
yapapahub <40893287+yapapahub@users.noreply.github.com>
2024-04-12 04:22:41 -0400

    support for _noelaborate tag

--- helpers/parser.js
+++ helpers/parser.js
@@ -22,6 +22,7 @@ module.exports = function (item) {
       //special tags check 
       if (item.tags.includes('_noads')) { temp.noads = true }
       if (item.tags.includes('_noaff')) { temp.noaff = true }
+      if (item.tags.includes('_noelaborate')) { temp.noelaborate = true }
       //tags filter and formating 
       temp.tags = item.tags
           .filter(tag=>tag.slice(0,1)!=="_")
