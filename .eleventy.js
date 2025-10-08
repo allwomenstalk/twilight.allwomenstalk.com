903aa71c3a3e0e0095b326f98fe9cb4bf70bfcf8
yapapahub <40893287+yapapahub@users.noreply.github.com>
2025-10-08 19:22:52 -0400

    list filter for posts.njk

--- .eleventy.js
+++ .eleventy.js
@@ -53,6 +53,63 @@ module.exports = function(eleventyConfig) {
     }
   });
 
+  eleventyConfig.addFilter("enhanceLists", function(content) {
+    if (typeof content !== "string" || !content.includes("<ul")) {
+      return content;
+    }
+
+    const wrapperOpen = '<div class="overflow-hidden bg-white shadow rounded-md sm:rounded-lg dark:bg-gray-800/50 dark:shadow-none dark:outline dark:outline-1 dark:-outline-offset-1 dark:outline-white/10">';
+    const classRegex = /class=(["'])(.*?)\1/;
+    const roleRegex = /\brole\s*=\s*["'][^"']*["']/;
+    const baseUlClasses = [
+      "not-prose",
+      "list-none",
+      "divide-y",
+      "divide-gray-200",
+      "dark:divide-white/10"
+    ];
+    const baseLiClasses = [
+      "px-4",
+      "py-4",
+      "sm:px-6"
+    ];
+
+    const mergeClasses = (attrString, classesToAdd) => {
+      if (classRegex.test(attrString)) {
+        const match = attrString.match(classRegex);
+        const delimiter = match[1];
+        const existingClasses = match[2].split(/\s+/).filter(Boolean);
+        const classSet = new Set(existingClasses);
+        classesToAdd.forEach(cls => classSet.add(cls));
+        attrString = attrString.replace(classRegex, `class=${delimiter}${Array.from(classSet).join(" ")}${delimiter}`);
+      } else {
+        attrString += ` class="${classesToAdd.join(" ")}"`;
+      }
+      return attrString;
+    };
+
+    let enhanced = content.replace(/<ul(\s[^>]*)?>/g, (match, attrs = "") => {
+      let attrString = attrs || "";
+      attrString = mergeClasses(attrString, baseUlClasses);
+
+      if (!roleRegex.test(attrString)) {
+        attrString += ' role="list"';
+      }
+
+      return `${wrapperOpen}\n  <ul${attrString}>`;
+    });
+
+    enhanced = enhanced.replace(/<\/ul>/g, '  </ul>\n</div>');
+
+    enhanced = enhanced.replace(/<li(\s[^>]*)?>/g, (match, attrs = "") => {
+      let attrString = attrs || "";
+      attrString = mergeClasses(attrString, baseLiClasses);
+      return `<li${attrString}>`;
+    });
+
+    return enhanced;
+  });
+
   // Watch targets
   eleventyConfig.addWatchTarget("./src/styles/");
 
